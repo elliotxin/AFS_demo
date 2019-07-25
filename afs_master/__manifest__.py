86bd8fdc2827af2af7a59e063d8f667bcb58e307
dominique kon-sun-tack <dominique.k@elico-corp.com.sg>
2019-07-25 23:12:38 +0800

    [UPG] afs_master
    misc changes on the views

--- afs_master/__manifest__.py
+++ afs_master/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Partner Data for Alliance Francaise Singapore',
-    'version': '12.0.0.1.1',
+    'version': '12.0.0.1.2',
     'category': 'AFS',
     'author': 'Alitec Pte Ltd',
     'license': 'AGPL-3',
