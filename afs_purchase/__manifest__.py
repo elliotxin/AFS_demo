f903819640dfb7804f3711e9d352763ac908981b
dominique kon-sun-tack <dominique.k@elico-corp.com.sg>
2019-08-22 10:36:38 +0800

    Auto stash before rebase of "master"

---
+++ afs_purchase/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 ALitec Pte Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Purchase Import Module for Alliance Francaise Singapore',
+    'version': '12.0.0.1.00',
+    'category': 'AFS',
+    'author': 'Alitec Pte Ltd',
+    'license': 'AGPL-3',
+    'summary': 'Purchase History module',
+    'data': [
+    ],
+    'depends': [],
+    'installable': True,
+}
